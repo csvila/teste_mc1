7aa57621a6da6f2273d3b9d0d05b300c7fdcc1f6
Richar Flores <richoff@gmail.com>
2019-11-24 12:27:55 -0300

    fibonacci

---
+++ fibonacci/Fibo/Fibo/Calculos.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Fibo
+{
+    public class Calculos
+    {
+        public  long LoopFibonacci(int number)
+        {
+
+            long previous = 0;
+            long current = 1;
+            long fibo = 1;
+            if (number == 0)
+                return 0;
+            if (number == 1)
+                return fibo;
+
+            for (long i = 2; i <= number; i++)
+            {
+                fibo = current + previous;
+                previous = current;
+                current = fibo;
+            }
+            return fibo;
+        }
+        public  long RecursiveFibonacci(int number)
+        {
+
+            if (number == 0)
+                return 0;
+            if (number == 1)
+                return 1;
+            return RecursiveFibonacci(number - 1) + RecursiveFibonacci(number - 2);
+
+        }
+    }
+}
---
+++ fibonacci/Fibo/Fibo/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Fibo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculos calculador = new Calculos();
+            Console.WriteLine("Digite um numero para calculo da sequência Fibo");
+            int number;
+            number = Convert.ToInt32(Console.ReadLine());
+            Stopwatch lSw = new Stopwatch();
+            lSw.Start();
+            long loop = calculador.LoopFibonacci(number);
+            lSw.Stop();
+            Stopwatch rSw = new Stopwatch();
+            rSw.Start();
+            long recursive = calculador.RecursiveFibonacci(number);
+            rSw.Stop();
+            Console.WriteLine($"LoopFibonacci: {loop} ElapsedTime: {lSw.Elapsed} ");
+            Console.WriteLine($"RecursiveFibonacci: {recursive} ElapsedTime: {rSw.Elapsed} ");
+            Console.ReadKey();
+
+        }
+       
+    }
+}
